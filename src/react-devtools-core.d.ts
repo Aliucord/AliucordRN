c167a891fc27a549008ce9a6ea66011dfc57d13f
TymanWasTaken <32660892+TymanWasTaken@users.noreply.github.com>
2022-04-15 12:24:24 -0600

    add reactdevtools-core typings

---
+++ src/react-devtools-core.d.ts
@@ -0,0 +1,20 @@
+declare module "react-devtools-core" {
+    type ConnectionOptions = {
+        host?: string,
+        nativeStyleEditorValidAttributes?: string[],
+        port?: number,
+        useHttps?: boolean,
+        resolveRNStyle?: (key: number) => Record<string, unknown>,
+        retryConnectionDelay?: number,
+        isAppActive?: () => boolean,
+        websocket?: WebSocket
+    
+    }
+
+    export const connectToDevTools: (options: ConnectionOptions) => void;
+
+    const exports: {
+        connectToDevTools: (options: ConnectionOptions) => void
+    };
+    export default exports;
+}
