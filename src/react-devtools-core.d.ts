b48ffc38d2eeb9483e2134c5d8ce08e47df9d514
TymanWasTaken <32660892+TymanWasTaken@users.noreply.github.com>
2022-04-15 13:54:17 -0600

    Add react-devtools-core typings (#2)
    * add reactdevtools-core typings
    
    * whitespace moment

---
+++ src/react-devtools-core.d.ts
@@ -0,0 +1,19 @@
+declare module "react-devtools-core" {
+    type ConnectionOptions = {
+        host?: string,
+        nativeStyleEditorValidAttributes?: string[],
+        port?: number,
+        useHttps?: boolean,
+        resolveRNStyle?: (key: number) => Record<string, unknown>,
+        retryConnectionDelay?: number,
+        isAppActive?: () => boolean,
+        websocket?: WebSocket
+    }
+
+    export const connectToDevTools: (options: ConnectionOptions) => void;
+
+    const exports: {
+        connectToDevTools: (options: ConnectionOptions) => void
+    };
+    export default exports;
+}
