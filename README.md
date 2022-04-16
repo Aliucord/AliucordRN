a20df6849f8a0377ed02e6790a74e5ad085a1093
Vendicated <vendicated@riseup.net>
2022-04-16 03:09:17 +0200

    Add serve script

--- README.md
+++ README.md
@@ -14,15 +14,4 @@ pnpm build
 Kinda inconvenient for now, will be improved in the future
 
 1) Install and enable the [Xposed Module](https://github.com/Aliucord/AliucordXposed/releases/download/1.0.0/aliuxposed.apk). Make sure you check Discord in application list
-2) Host the build on localhost:3000/Aliucord.js on your phone
-   For example via Termux:
-   ```sh
-   # on PC 
-   adb push dist/Aliucord.js /sdcard
-
-   # in Termux
-   mkdir ajs
-   cd ajs
-   mv /sdcard/Aliucord.js .
-   python -m http.server 3000
-   ```
+2) Connect your phone via adb and run `pnpm serve`
---
+++ serve.mjs
@@ -0,0 +1,27 @@
+import { createServer } from "http";
+import { readFile } from "fs";
+import { spawn } from "child_process";
+
+// http-server exists but it is so bloated 😩
+
+createServer((req, res) => {
+    console.info("-> Received Request for", req.url);
+    if (req.url !== "/Aliucord.js") res.writeHead(404).end();
+    else {
+        readFile("dist/Aliucord.js", { encoding: "utf-8" }, (err, data) => {
+            if (err) {
+                console.error(err);
+                res.writeHead(500);
+            } else {
+                res.writeHead(200, { "Content-Type": "text/javascript" });
+                res.write(data);
+            }
+            res.end();
+        });
+    }
+})
+    .once("listening", () => console.info("Listening on Port 3000"))
+    .on("error", console.error)
+    .listen(3000);
+
+spawn("adb", ["reverse", "tcp:3000", "tcp:3000"], { stdio: "inherit" });
