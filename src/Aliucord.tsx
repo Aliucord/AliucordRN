27848ab85e868dac8ccac6fb98a7eee2bd42ed83
TymanWasTaken <32660892+TymanWasTaken@users.noreply.github.com>
2022-04-30 15:01:10 -0600

    Remove re-exporting

--- src/Aliucord.tsx
+++ src/Aliucord.tsx
@@ -20,8 +20,6 @@ interface SettingsSchema {
 
 export let settings: Settings<SettingsSchema>;
 export const logger = new Logger("AliucordMain");
-export * as Metro from "./metro";
-export * as Patcher from "./utils/patcher";
 export async function load() {
     settings = await Settings.make("Aliucord");
 
