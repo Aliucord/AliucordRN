9cf2ed5e1cd72be3ca1bdfa545089bbcdd2ca294
TymanWasTaken <32660892+TymanWasTaken@users.noreply.github.com>
2022-05-09 11:58:44 -0600

    Request permissions in bootstrap

--- bootstrap/index.ts
+++ bootstrap/index.ts
@@ -1,6 +1,5 @@
 (async () => {
-    try {
-        const { externalStorageDirectory, download } = nativeModuleProxy.AliucordNative;
+    async function loadAliucord() {
         const bundlePath = externalStorageDirectory + "/AliucordRN/Aliucord.js.bundle";
         if (!AliuFS.exists(bundlePath)) {
             try {
@@ -12,6 +11,22 @@
         }
 
         (globalThis._globals ??= {}).aliucord = AliuHermes.run(bundlePath);
+    }
+    try {
+        const { externalStorageDirectory, requestPermissions, download, checkPermissions } = nativeModuleProxy.AliucordNative;
+        const granted = await checkPermissions()
+        if (!granted) Metro.ReactNative.Alert.alert(
+            "Storage Access",
+            "Aliucord needs access to your storage to load plugins and themes.",
+            [{
+                text: "OK",
+                onPress: () => requestPermissions().then(permissionGranted => {
+                    if (permissionGranted) loadAliucord();
+                    else alert("Unable to load, Aliucord needs access to your storage to load plugins and themes.");
+                })
+            }]
+        );
+        else await loadAliucord();
     } catch (error) {
         alert("Something went wrong :(\nCheck logs");
         console.error((error as Error).stack);
