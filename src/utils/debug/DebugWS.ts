8859f7d53e5f3937cd7c3da32e2d681c89608dbb
TymanWasTaken <32660892+TymanWasTaken@users.noreply.github.com>
2022-05-10 10:28:13 -0600

    Readd dev ws changes

--- src/utils/debug/DebugWS.ts
+++ src/utils/debug/DebugWS.ts
@@ -5,14 +5,18 @@ import { before } from "../patcher";
 const logger = new Logger("DebugWS");
 
 export class DebugWS {
-    socket: WebSocket | undefined;
+    socket: WebSocket | null = null;
+    patched: boolean = false;
 
     start() {
+        if (this.socket) return;
+        this.socket = new WebSocket("ws://localhost:3000");
+
+        const logger = new Logger("DebugWS");
         logger.info("Connecting to debug ws");
-        this.socket = new WebSocket("ws://localhost:9090");
 
         this.socket.addEventListener("open", () => logger.info("Connected with debug websocket"));
-        this.socket.addEventListener("error", e => logger.error((e as any).message));
+        this.socket.addEventListener("error", e => logger.error((e as ErrorEvent).message));
         this.socket.addEventListener("message", async message => {
             try {
                 const { data } = message;
@@ -25,11 +29,19 @@ export class DebugWS {
                 logger.error(e as Error | string);
             }
         });
-
-        before(globalThis, "nativeLoggingHook", (_, message: string, level: number) => {
-            if (this.socket?.readyState === WebSocket.OPEN) {
-                this.socket.send(JSON.stringify({ level, message }));
-            }
+        this.socket.addEventListener("close", () => {
+            logger.info("Disconnected from debug websocket, reconnecting in 3 seconds");
+            this.socket = null;
+            setTimeout(this.start, 3000);
         });
+
+        if (!this.patched) {
+            before(globalThis, "nativeLoggingHook", (_, message: string, level: number) => {
+                if (this.socket?.readyState === WebSocket.OPEN) {
+                    this.socket.send(JSON.stringify({ level, message }));
+                }
+            });
+            this.patched = true;
+        }
     }
 }
