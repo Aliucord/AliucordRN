8c9a70b9b9d667d275093e5ee6dd138e8b2632c2
TymanWasTaken <32660892+TymanWasTaken@users.noreply.github.com>
2022-05-10 10:28:11 -0600

    Fix bootstrap script (#17)

--- bootstrap/index.d.ts
+++ bootstrap/index.d.ts
@@ -3,6 +3,30 @@ declare global {
         AliucordNative: {
             externalStorageDirectory: string,
             download: (url: string, path: string) => Promise<void>;
+            requestPermissions: () => Promise<boolean>;
+            checkPermissions: () => Promise<boolean>;
+        };
+        DialogManagerAndroid: {
+            getConstants: () => {
+                buttonClicked: string;
+                dismissed: string;
+                buttonPositive: number;
+                buttonNegative: number;
+                buttonNeutral: number;
+            };
+            showAlert: (
+                config: {
+                    title?: string,
+                    message?: string,
+                    buttonPositive?: string,
+                    buttonNegative?: string,
+                    buttonNeutral?: string,
+                    items?: string[],
+                    cancelable?: boolean,
+                },
+                onError: (error: Error) => any,
+                onAction: (action: string, key: number) => any
+            ) => void;
         };
     };
 }
--- bootstrap/index.ts
+++ bootstrap/index.ts
@@ -1,6 +1,6 @@
 (async () => {
-    try {
-        const { externalStorageDirectory, download } = nativeModuleProxy.AliucordNative;
+    const { externalStorageDirectory, requestPermissions, download, checkPermissions } = nativeModuleProxy.AliucordNative;
+    async function downloadAliucord() {
         const bundlePath = externalStorageDirectory + "/AliucordRN/Aliucord.js.bundle";
         if (!AliuFS.exists(bundlePath)) {
             try {
@@ -12,8 +12,39 @@
         }
 
         (globalThis._globals ??= {}).aliucord = AliuHermes.run(bundlePath);
+    }
+    try {
+        const granted = await checkPermissions();
+        const constants = nativeModuleProxy.DialogManagerAndroid.getConstants();
+        if (!granted) {
+            const dialogResult = await new Promise<boolean>((res, rej) => {
+                nativeModuleProxy.DialogManagerAndroid.showAlert({
+                    title: "Storage Permissions",
+                    message: "Aliucord needs access to your storage to load plugins and themes.",
+                    cancelable: true,
+                    buttonPositive: "Ok",
+                    buttonNegative: "Cancel"
+                }, rej, (action, key) => {
+                    if (action === constants.buttonClicked && key === constants.buttonPositive) res(true);
+                    else res(false);
+                });
+            });
+            if (!(dialogResult && await requestPermissions())) {
+                nativeModuleProxy.DialogManagerAndroid.showAlert({
+                    title: "Storage Permissions",
+                    message: "Access to your storage is required for aliucord to load.",
+                    cancelable: false
+                }, () => { }, () => { });
+                return;
+            }
+        }
+        await downloadAliucord();
     } catch (error) {
-        alert("Something went wrong :(\nCheck logs");
+        nativeModuleProxy.DialogManagerAndroid.showAlert({
+            title: "Error",
+            message: "Something went wrong while loading aliucord, check logs for the specific error.",
+            cancelable: false
+        }, () => { }, () => { });
         console.error((error as Error).stack);
     }
 })();
