de396e1164cb003ac1aac62ee665370685b2d782
TymanWasTaken <32660892+TymanWasTaken@users.noreply.github.com>
2022-05-12 23:39:20 -0600

    remove perms check in normal aliucord bundle

--- src/Aliucord.ts
+++ src/Aliucord.ts
@@ -1,19 +1,12 @@
 import { PluginManager } from "./api";
 import { Settings } from "./api/Settings";
 import * as CorePlugins from "./core-plugins/index";
-import * as Metro from "./metro";
 import { mkdir } from "./native/fs";
-import { checkPermissions, requestPermissions } from "./native/permissions";
 import patchSettings from "./ui/patchSettings";
-import { ALIUCORD_DIRECTORY, PLUGINS_DIRECTORY, SETTINGS_DIRECTORY } from "./utils/constants";
+import { PLUGINS_DIRECTORY, SETTINGS_DIRECTORY } from "./utils/constants";
 import { DebugWS } from "./utils/debug/DebugWS";
 import { ReactDevTools } from "./utils/debug/ReactDevTools";
 import { Logger } from "./utils/Logger";
-
-function initWithPerms() {
-    if (!AliuFS.exists(PLUGINS_DIRECTORY)) AliuFS.mkdir(PLUGINS_DIRECTORY);
-    if (!AliuFS.exists(SETTINGS_DIRECTORY)) AliuFS.mkdir(SETTINGS_DIRECTORY);
-}
 
 interface SettingsSchema {
     autoUpdateAliucord: boolean;
@@ -34,27 +27,8 @@ export class Aliucord {
         try {
             this.logger.info("Loading...");
 
-            // TODO move to bootstrap
-            checkPermissions().then(granted => {
-                if (granted) initWithPerms();
-                else {
-                    Metro.ReactNative.Alert.alert(
-                        "Storage Access",
-                        "Aliucord needs access to your storage to load plugins and themes.",
-                        [{
-                            text: "OK",
-                            onPress: () => requestPermissions().then(permissionGranted => {
-                                if (permissionGranted) initWithPerms();
-                                else alert("Aliucord needs access to your storage to load plugins and themes.");
-                            })
-                        }]
-                    );
-                }
-            });
-
-            mkdir(ALIUCORD_DIRECTORY);
-            mkdir(SETTINGS_DIRECTORY);
             mkdir(PLUGINS_DIRECTORY);
+            mkdir(SETTINGS_DIRECTORY);
 
             this.settings = await Settings.make("Aliucord");
 
