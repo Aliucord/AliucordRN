1e2fe7f12df58b5f1628d782823e999922344ebf
C10udburst <18114966+C10udburst@users.noreply.github.com>
2022-05-20 19:43:28 +0200

    fix silly url typo

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 import { externalStorageDirectory } from "../native/fs";
 
 export const ALIUCORD_INVITE = "https://discord.gg/EsNDvBaHVU";
-export const ALIUCORD_GITHUB = "https://github.com/Aliucord/Aliucord";
+export const ALIUCORD_GITHUB = "https://github.com/Aliucord/AliucordRN";
 export const ALIUCORD_PATREON = "https://patreon.com/Aliucord";
 
 export const ALIUCORD_DIRECTORY = externalStorageDirectory + "/AliucordRN/";
