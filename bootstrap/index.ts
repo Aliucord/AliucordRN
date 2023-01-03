1be6cb900f5be5c530ca3422c1e0ee3143dc4115
janisslsm <97699850+janisslsm@users.noreply.github.com>
2023-01-04 00:29:19 +0200

    fix checking for wrong file in commit history

--- bootstrap/index.ts
+++ bootstrap/index.ts
@@ -37,7 +37,7 @@
             return;
         }
 
-        const commits = await fetch("https://api.github.com/repos/Aliucord/AliucordRN/commits?sha=builds&path=bootstrap.js&per_page=1");
+        const commits = await fetch("https://api.github.com/repos/Aliucord/AliucordRN/commits?sha=builds&path=Aliucord.js.bundle&per_page=1");
         if (commits.status === 403) {
             const internalBundlePath = `${codeCacheDirectory}/Aliucord.js.bundle`;
             if (!AliuFS.exists(internalBundlePath)) await download("https://raw.githubusercontent.com/Aliucord/AliucordRN/builds/Aliucord.js.bundle", internalBundlePath);
