45e8a90fe16d39122598670f1b6f87864ef7e11c
janisslsm <97699850+janisslsm@users.noreply.github.com>
2023-01-04 20:02:31 +0200

    fix stupit single quote

--- src/metro/index.ts
+++ src/metro/index.ts
@@ -279,7 +279,7 @@ export enum AMOLEDThemeState {
 }
 
 export const setAMOLEDThemeEnabledBypass = (state) => {
-    FluxDispatcher.dispatch({ type: 'UNSYNCED_USER_SETTINGS_UPDATE', settings: { useAMOLEDTheme: state ? AMOLEDThemeState.ON : AMOLEDThemeState.OFF } });
+    FluxDispatcher.dispatch({ type: "UNSYNCED_USER_SETTINGS_UPDATE", settings: { useAMOLEDTheme: state ? AMOLEDThemeState.ON : AMOLEDThemeState.OFF } });
 };
 
 export const Dialog = getByProps("show", "openLazy", "confirm", "close") as {
