46ec81ab40544c8ebb051006aca331f37b79ee8b
nikosszzz <19nikospap@gmail.com>
2023-01-05 21:35:49 +0200

    themer: use try/catch

--- src/api/Themer.ts
+++ src/api/Themer.ts
@@ -22,63 +22,71 @@ export function setTheme(theme: Theme) {
 }
 
 export function applyTheme() {
-    if (currentTheme === undefined) return;
-    setAMOLEDThemeEnabledBypass(false);
-    for (const key in Constants.ThemeColorMap) {
-        Constants.ThemeColorMap[key][ThemeType.AMOLED] = Constants.ThemeColorMap[key][ThemeType.DARK];
-        if (currentTheme.theme_color_map[key]) {
-            Constants.ThemeColorMap[key][ThemeType.AMOLED] = currentTheme.theme_color_map[key]?.[ThemeType.DARK];
-            Constants.ThemeColorMap[key][ThemeType.LIGHT] = currentTheme.theme_color_map[key]?.[ThemeType.LIGHT];
-            Constants.ThemeColorMap[key][ThemeType.DARK] = currentTheme.theme_color_map[key]?.[ThemeType.DARK];
+    try {
+        if (currentTheme === undefined) return;
+        setAMOLEDThemeEnabledBypass(false);
+        for (const key in Constants.ThemeColorMap) {
+            Constants.ThemeColorMap[key][ThemeType.AMOLED] = Constants.ThemeColorMap[key][ThemeType.DARK];
+            if (currentTheme.theme_color_map[key]) {
+                Constants.ThemeColorMap[key][ThemeType.AMOLED] = currentTheme.theme_color_map[key]?.[ThemeType.DARK];
+                Constants.ThemeColorMap[key][ThemeType.LIGHT] = currentTheme.theme_color_map[key]?.[ThemeType.LIGHT];
+                Constants.ThemeColorMap[key][ThemeType.DARK] = currentTheme.theme_color_map[key]?.[ThemeType.DARK];
+            }
         }
-    }
 
-    for (const key in currentTheme.colors) {
-        Constants.Colors[key] = currentTheme.colors[key];
-    }
+        for (const key in currentTheme.colors) {
+            Constants.Colors[key] = currentTheme.colors[key];
+        }
 
-    if (currentTheme.unsafe_colors && !currentTheme.theme_color_map["CHAT_BACKGROUND"]) {
-        Constants.ThemeColorMap.CHAT_BACKGROUND[ThemeType.AMOLED] = currentTheme.theme_color_map["BACKGROUND_PRIMARY"][ThemeType.DARK];
-        Constants.ThemeColorMap.CHAT_BACKGROUND[ThemeType.LIGHT] = currentTheme.theme_color_map["BACKGROUND_PRIMARY"][ThemeType.LIGHT];
-        Constants.ThemeColorMap.CHAT_BACKGROUND[ThemeType.DARK] = currentTheme.theme_color_map["BACKGROUND_PRIMARY"][ThemeType.DARK];
-    }
+        if (currentTheme.unsafe_colors && !currentTheme.theme_color_map["CHAT_BACKGROUND"]) {
+            Constants.ThemeColorMap.CHAT_BACKGROUND[ThemeType.AMOLED] = currentTheme.theme_color_map["BACKGROUND_PRIMARY"][ThemeType.DARK];
+            Constants.ThemeColorMap.CHAT_BACKGROUND[ThemeType.LIGHT] = currentTheme.theme_color_map["BACKGROUND_PRIMARY"][ThemeType.LIGHT];
+            Constants.ThemeColorMap.CHAT_BACKGROUND[ThemeType.DARK] = currentTheme.theme_color_map["BACKGROUND_PRIMARY"][ThemeType.DARK];
+        }
 
-    setAMOLEDThemeEnabledBypass(true);
-    logger.info("Theme applied: ", currentTheme.name);
+        setAMOLEDThemeEnabledBypass(true);
+        logger.info("Theme applied: ", currentTheme.name);
+    } catch (e) {
+        logger.error("Failed to apply theme: ", e);
+    }
 }
 
 export function themerInit() {
-    // Only those two are used
-    AliuHermes.unfreeze(Constants.ThemeColorMap);
-    AliuHermes.unfreeze(Constants.Colors);
+    try {
+        // Only those two are used
+        AliuHermes.unfreeze(Constants.ThemeColorMap);
+        AliuHermes.unfreeze(Constants.Colors);
 
-    for (const file of readdir(THEME_DIRECTORY)) {
-        if (!file.name.endsWith(".json")) continue;
+        for (const file of readdir(THEME_DIRECTORY)) {
+            if (!file.name.endsWith(".json")) continue;
 
-        const themeFile = JSON.parse(AliuFS.readFile(THEME_DIRECTORY + file.name, "text") as string) as Theme;
-        themes[themeFile.name] = themeFile;
-    }
+            const themeFile = JSON.parse(AliuFS.readFile(THEME_DIRECTORY + file.name, "text") as string) as Theme;
+            themes[themeFile.name] = themeFile;
+        }
 
-    if (themes[window.Aliucord.settings.get("theme", "")] !== undefined) {
-        currentTheme = themes[window.Aliucord.settings.get("theme", "")];
+        if (themes[window.Aliucord.settings.get("theme", "")] !== undefined) {
+            currentTheme = themes[window.Aliucord.settings.get("theme", "")];
 
-        // Chat Input background
-        after(getByName("ChatInput").default.prototype, "render", (_, comp) => {
-            if (currentTheme === undefined) return;
+            // Chat Input background
+            after(getByName("ChatInput").default.prototype, "render", (_, comp) => {
+                if (currentTheme === undefined) return;
 
-            comp.props.children[2].props.children.props.style[0].backgroundColor = currentTheme.theme_color_map["BACKGROUND_MOBILE_SECONDARY"][ThemeType.DARK];
-        });
+                comp.props.children[2].props.children.props.style[0].backgroundColor = currentTheme.theme_color_map["BACKGROUND_MOBILE_SECONDARY"][ThemeType.DARK];
+            });
 
-        // Navigation Bar
-        after(getByName("ChannelSafeAreaBottom"), "default", (_, comp) => {
-            if (currentTheme === undefined) return;
+            // Navigation Bar
+            after(getByName("ChannelSafeAreaBottom"), "default", (_, comp) => {
+                if (currentTheme === undefined) return;
 
-            comp.props.style.backgroundColor = currentTheme.theme_color_map["BACKGROUND_MOBILE_SECONDARY"][ThemeType.DARK];
-        });
+                comp.props.style.backgroundColor = currentTheme.theme_color_map["BACKGROUND_MOBILE_SECONDARY"][ThemeType.DARK];
+            });
 
-        applyTheme();
-    } else {
-        window.Aliucord.settings.set("theme", "");
+            applyTheme();
+        } else {
+            window.Aliucord.settings.set("theme", "");
+        }
+    } catch (e) {
+        logger.error(e);
     }
 } 
 
