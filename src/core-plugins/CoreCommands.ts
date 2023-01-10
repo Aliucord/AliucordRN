87fc72989babb53d058a5796ce8bb1013be0bd2b
OmegaSunkey <94485936+OmegaSunkey@users.noreply.github.com>
2023-01-09 23:21:32 -0500

    Suggested changes done

--- src/core-plugins/CoreCommands.ts
+++ src/core-plugins/CoreCommands.ts
@@ -7,7 +7,7 @@ import { DebugInfo } from "../utils/debug/DebugInfo";
 import { makeAsyncEval } from "../utils/misc";
 import { exists, externalStorageDirectory } from "../native/fs";
 
-const customBundle: boolean = AliuFS.exists(externalStorageDirectory + "/AliucordRN/Aliucord.js.bundle");
+const customBundle = AliuFS.exists(externalStorageDirectory + "/AliucordRN/Aliucord.js.bundle");
 
 export default class CoreCommands extends Plugin {
     start() {
@@ -86,7 +86,8 @@ export default class CoreCommands extends Plugin {
                 MessageActions.sendMessage(ctx.channel.id, {
                     content: `**Debug Info:**
                         > Discord: ${DebugInfo.discordVersion}
-                        > Aliucord: ${sha} (${Object.keys(plugins).length} plugins) Custom Bundle: ${customBundle}
+                        > Aliucord: ${sha} (${Object.keys(plugins).length} plugins 
+                        > Custom Bundle: ${customBundle}
                         > System: ${DebugInfo.system}
                         > React: ${DebugInfo.reactVersion}
                         > Hermes: ${DebugInfo.hermesVersion}
